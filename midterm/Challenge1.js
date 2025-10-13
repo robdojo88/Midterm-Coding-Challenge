80a062ee757b95d9a40c6203f74ad6791ae83989
robdojo88 <robert.dojo.88@gmail.com>
2025-10-13 11:08:00 +0800

    coding challenge 1

---
+++ midterm/Challenge1.js
@@ -0,0 +1,39 @@
+/* ************************************************************
+
+^#1 Create Hello World
+
+https://leetcode.com/problems/create-hello-world-function/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#2 Counter
+
+https://leetcode.com/problems/counter/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#3 To Be Or Not To Be
+
+https://leetcode.com/problems/counter/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#4 Counter II
+
+https://leetcode.com/problems/counter-ii/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#5 Apply Transform Over Each Element in Array
+
+https://leetcode.com/problems/apply-transform-over-each-element-in-array/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
