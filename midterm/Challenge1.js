ce1a28fd1b693e8b1e22a6ebcf911b24f46dc80e
robdojo88 <robert.dojo.88@gmail.com>
2025-10-13 11:26:43 +0800

    coding challenge 1

--- midterm/Challenge1.js
+++ midterm/Challenge1.js
@@ -37,3 +37,43 @@ https://leetcode.com/problems/counter-ii/?envType=study-plan-v2&envId=30-days-of
 https://leetcode.com/problems/apply-transform-over-each-element-in-array/?envType=study-plan-v2&envId=30-days-of-javascript
 
 */
+
+/* ************************************************************
+
+^#6 Filter Elements from Array
+
+https://leetcode.com/problems/filter-elements-from-array/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#7 Array Reduce Transformation
+
+https://leetcode.com/problems/array-reduce-transformation/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#8 Function Composition
+
+https://leetcode.com/problems/function-composition/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#9 Return Length of Arguments Passed
+
+https://leetcode.com/problems/return-length-of-arguments-passed/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
+
+/* ************************************************************
+
+^#10 Allow One Function Call
+
+https://leetcode.com/problems/allow-one-function-call/?envType=study-plan-v2&envId=30-days-of-javascript
+
+*/
